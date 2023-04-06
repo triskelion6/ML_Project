77351275192c06a8a67b9104432e21008b5073a0
Sharathkumar_M <sharath.malavalli8@gmail.com>
2023-04-06 17:15:34 +0530

    Model Trainer

--- src/logger.py
+++ src/logger.py
@@ -10,7 +10,7 @@ LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
 
 logging.basicConfig(
     filename = LOG_FILE_PATH,
-    format = "[ %(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format = "[ %(asctime)s] (%(filename)s : %(lineno)d) %(name)s - %(levelname)s - %(message)s ",
     level= logging.INFO,  
 )
 
